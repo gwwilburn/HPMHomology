f5533b853f5b675bdd9d32d6038089789db0fcd1
Grey Wilburn <wilburn@g.harvard.edu>
2018-11-07 18:05:14 -0500

    initial commit of skeleton code

---
+++ src/hpmscoreIS.c
@@ -0,0 +1,16 @@
+/* hpmscoreIS.c */
+/* infant stages of scoring sequences with Potts models using importance samplign and HMM's */
+
+#include "p7_config.h"
+#include "easel.h"
+#include "esl_alphabet.h"
+#include "esl_sq.h"
+#include "esl_sqio.h"
+
+#include "hmmer.h"
+
+
+int main(int argc, char **argv){
+	fprintf(stdout, "hello world\n");
+	return 0;
+}
