a7a1e7d53288188a00b23e2829b356e7c09d276b
gwwilburn <wilburn@g.harvard.edu>
2020-04-10 20:13:40 -0400

    fixed tabs in hpm.h

--- src/hpm.h
+++ src/hpm.h
@@ -12,10 +12,10 @@
 #include "potts.h"
 
 enum hpm_transitions {
-	HPM_MM = 0,
-	HPM_MI = 1,
-	HPM_IM = 2,
-	HPM_II = 3,
+   HPM_MM = 0,
+   HPM_MI = 1,
+   HPM_IM = 2,
+   HPM_II = 3,
 };
 
 #define HPM_NTRANSITIONS 4
@@ -27,26 +27,22 @@ enum hpm_transitions {
 
 
 typedef struct hpm_s{
-
-	int		  		 M;				/* number of nodes in model 					*/
-	double 		  **t;				/* transition probs 								*/
-	double	 	  **ins;	      	/* insert emission probs					   */
-	float    	  **h;				/* site-specific h_i params					*/
-	float   		 ***e;				/* coupling e_ij params							*/
-	int				 nTransition;  /* number of transitions, should be 4 		*/
-
-	ESL_ALPHABET *abc;            /* alphabet											*/
-
+   int              M;            /* number of nodes in model            */
+   double         **t;            /* transition probs                    */
+   double         **ins;          /* insert emission probs               */
+   float          **h;            /* site-specific h_i params            */
+   float         ***e;            /* coupling e_ij params                */
+   int              nTransition;  /* number of transitions, should be 4  */
+   ESL_ALPHABET    *abc;          /* alphabet                            */
 } HPM;
 
 
-/* hpm.c	*/
-extern HPM	         *hpm_Create(int M, ESL_ALPHABET *abc);
-extern void           hpm_Destroy(HPM *hpm);
-extern HPM 				*hpm_Create_hmm_potts(P7_HMM *hmm, POTTS *potts, ESL_ALPHABET *abc);
-extern HPM 				*hpm_Create_3mer(ESL_ALPHABET *abc);
-extern P7_HMM 		   *hmm_Create_3mer(ESL_ALPHABET *abc);
-extern int            IDX(int i, int j, int K);
-
+/* hpm.c */
+extern HPM       *hpm_Create(int M, ESL_ALPHABET *abc);
+extern void       hpm_Destroy(HPM *hpm);
+extern HPM       *hpm_Create_hmm_potts(P7_HMM *hmm, POTTS *potts, ESL_ALPHABET *abc);
+extern HPM       *hpm_Create_3mer(ESL_ALPHABET *abc);
+extern P7_HMM    *hmm_Create_3mer(ESL_ALPHABET *abc);
+extern int        IDX(int a, int b, int K);
 
 #endif
