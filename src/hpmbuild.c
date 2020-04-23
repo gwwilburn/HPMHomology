d2bc0ae54a2ba367bdcdaa8b296ef233f0106f76
gwwilburn <wilburn@g.harvard.edu>
2020-04-23 18:28:21 -0400

    fixed memory leaks and retabbed hpmbuild

--- src/hpmbuild.c
+++ src/hpmbuild.c
@@ -14,27 +14,21 @@
 #include "potts.h"
 #include "pottsfile.h"
 
-struct cfg_s { /* shared configuration in master and workers */
-   ESL_ALPHABET    *abc;
-	P7_HMMFILE      *hfp;      /* input hmm file                */
-   P7_HMM          *hmm;      /* input hmm                     */
-   HPM             *hpm;      /* hidden potts model            */
-	POTTS           *potts;    /* potts model                   */
-
-};
-
 static ESL_OPTIONS options[] = {
-   /* name         type        default env   range togs  reqs  incomp      help                                                   docgroup */
-   { "-h",         eslARG_NONE,  FALSE, NULL, NULL, NULL,NULL, NULL,            "help; show brief info on version and usage",              1 },
+   /* name       type         default env   range togs  reqs  incomp           help                                                  docgroup */
+   { "-h",       eslARG_NONE,  FALSE, NULL, NULL, NULL, NULL, NULL,            "help; show brief info on version and usage",           1 },
 
    /* Options forcing which alphabet we're working in (normally autodetected) */
-   { "--amino",  eslARG_NONE,  FALSE,  NULL, NULL, NULL,NULL,"--dna,--rna",   "<pottsfile> and <hmmfile> are for protein sequences",                    2 },
-   { "--dna",    eslARG_NONE,  FALSE, NULL, NULL, NULL,NULL,"--amino,--rna",  "<pottsfile> and <hmmfile> are for dna sequences",                        2 },
-   { "--rna",    eslARG_NONE,  FALSE, NULL, NULL, NULL,NULL,"--amino,--dna",  "<pottsfile> and <hmmfile> are for rna sequences",                        2 },
-	/* Option to create 3-mer dummy protein hpm */
-	{ "--3mer",  eslARG_NONE,  FALSE,  NULL, NULL, NULL,NULL,NULL,             "create dummy 3-mer hpm, usage = [-options] <hpmfile>",                   3 },
+   { "--amino",  eslARG_NONE,  FALSE, NULL, NULL, NULL, NULL, "--dna,--rna",   "<pottsfile> and <hmmfile> are for protein sequences",  2 },
+   { "--dna",    eslARG_NONE,  FALSE, NULL, NULL, NULL, NULL, "--amino,--rna", "<pottsfile> and <hmmfile> are for dna sequences",      2 },
+   { "--rna",    eslARG_NONE,  FALSE, NULL, NULL, NULL, NULL, "--amino,--dna", "<pottsfile> and <hmmfile> are for rna sequences",      2 },
+
+   /* Option to create 3-mer dummy protein hpm */
+   { "--3mer",   eslARG_NONE,  FALSE, NULL, NULL, NULL, NULL, NULL,            "create dummy 3-mer hpm, usage = [-options] <hpmfile>", 3 },
    { 0,0,0,0,0,0,0,0,0,0 },
 };
+
+static char banner[] = "Build an HPM from an HMM and a Potts model (w/ same number of match states)";
 static char usage[]  = "[-options] <hpmfile> <hmmfile> <pottsfile>";
 
 static void
@@ -46,87 +40,112 @@ cmdline_failure(char *argv0, char *format, ...)
    va_end(argp);
    esl_usage(stdout, argv0, usage);
    printf("\nTo see more help on available options, do %s -h\n\n", argv0);
+
    exit(1);
 }
 
 int main(int argc, char *argv[]) {
+   ESL_GETOPTS   *go;                             /* cmd line configuration    */
+   ESL_ALPHABET  *abc                    = NULL;  /* esl_alphabet              */
+   P7_HMMFILE    *hfp;                            /* input hmm file            */
+   P7_HMM        *hmm;                            /* input hmm                 */
+   POTTS         *potts;                          /* input potts model         */
+   HPM           *hpm;                            /* hpm to be created         */
+   char          *hpm_outfile            = NULL;  /* hpm output file path      */
+   char          *hmmfile                = NULL;  /* hmm file path             */
+   char          *pottsfile              = NULL;  /* hmm file path             */
+   FILE          *hpm_outfp              = NULL;  /* hpm output file object    */
+   int            status;                         /* easel return code         */
+   char           errbuf[eslERRBUFSIZE];          /* for error messages        */
 
-	ESL_GETOPTS    *go;                                            /* cmd line configuration        */
-   struct cfg_s    cfg;                                           /* application configuration     */
-   char           *hpm_outfile            = NULL;                 /* hpm output file path          */
-   char           *hmmfile                = NULL;                 /* hmm file path                 */
-   char           *pottsfile              = NULL;                 /* hmm file path                 */
- 	FILE           *hpm_outfp              = NULL;						/* hpm output file object        */
- 	int             status;                                        /* easel return code             */
-   char            errbuf[eslERRBUFSIZE];
+   /* parse command line */
+   go = esl_getopts_Create(options);
 
-	/* parse command line */
-	go = esl_getopts_Create(options);
    if (esl_opt_ProcessCmdline(go, argc, argv) != eslOK) cmdline_failure(argv[0], "Failed to parse command line: %s\n", go->errbuf);
    if (esl_opt_VerifyConfig(go)               != eslOK) cmdline_failure(argv[0], "Error in app configuration:   %s\n", go->errbuf);
 
-	/* check and see if we are making a dummy 3-mer model */
-	if       (esl_opt_GetBoolean(go, "--3mer")) {
-		/* we only want 1 argument */
-		if (esl_opt_ArgNumber(go)   != 1)     cmdline_failure(argv[0], "Incorrect number of command line arguments w/ dummy 3-mer hpm.\n", go->errbuf);
-		hpm_outfile    = esl_opt_GetArg(go, 1);
+   /* output help if requested */
+   if (esl_opt_GetBoolean(go, "-h") ) {
+      esl_banner(stdout, argv[0], banner);
+      esl_usage (stdout, argv[0], usage);
+      puts("\n where options are:");
+      esl_opt_DisplayHelp(stdout, go, 1, 2, 80);
+      puts("\n Alphabet options:");
+      esl_opt_DisplayHelp(stdout, go, 2, 2, 80);
+      puts("\n 3mer options:");
+      esl_opt_DisplayHelp(stdout, go, 3, 2, 80);
+      exit(0);
+   }
 
-		/* set alphabet to amino, forget what user says */
-		cfg.abc = esl_alphabet_Create(eslAMINO);
+   /* check and see if we are making a dummy 3-mer model */
 
-		/* make 3-mer hpm*/
-		cfg.hpm = hpm_Create_3mer(cfg.abc);
+   if (esl_opt_GetBoolean(go, "--3mer")) {
 
-		/* write to output file */
-		/* open hpm outfile for writing, write hpm to it */
-   	if ((hpm_outfp = fopen(hpm_outfile, "w")) == NULL) esl_fatal("Failed to open output hpm file %s for writing", hpm_outfile);
-   	hpmfile_Write(hpm_outfp, cfg.hpm);
+      /* we only want 1 argument */
+      if (esl_opt_ArgNumber(go) != 1) cmdline_failure(argv[0], "Incorrect number of command line arguments w/ dummy 3-mer hpm.\n", go->errbuf);
+      hpm_outfile = esl_opt_GetArg(go, 1);
 
-		return 0;
-	}
+      /* set alphabet to amino, forget what user says */
+      abc = esl_alphabet_Create(eslAMINO);
 
+      /* make 3-mer hpm*/
+      hpm = hpm_Create_3mer(abc);
 
-	if (esl_opt_ArgNumber(go)                  != 3)     cmdline_failure(argv[0], "Incorrect number of command line arguments.\n", go->errbuf);
+      /* write to output file */
+      /* open hpm outfile for writing, write hpm to it */
+      if ((hpm_outfp = fopen(hpm_outfile, "w")) == NULL) esl_fatal("Failed to open output hpm file %s for writing", hpm_outfile);
+      hpmfile_Write(hpm_outfp, hpm);
+      fclose(hpm_outfp);
 
-	hpm_outfile    = esl_opt_GetArg(go, 1);
-	hmmfile        = esl_opt_GetArg(go, 2);
-	pottsfile      = esl_opt_GetArg(go, 3);
+      /* clean up and return */
+      esl_alphabet_Destroy(abc);
+      hpm_Destroy(hpm);
+      esl_getopts_Destroy(go);
+
+      return 0;
+   }
+
+   /* normal, non-3mer mode of operation */
+   if (esl_opt_ArgNumber(go) != 3) cmdline_failure(argv[0], "Incorrect number of command line arguments.\n", go->errbuf);
+
+   hpm_outfile = esl_opt_GetArg(go, 1);
+   hmmfile     = esl_opt_GetArg(go, 2);
+   pottsfile   = esl_opt_GetArg(go, 3);
+
+   /* Set up the configuration structure shared amongst functions here */
+   /* Determine alphabet: do what user says, else autodetermine w/ hmm */
+   if      (esl_opt_GetBoolean(go, "--amino"))    abc = esl_alphabet_Create(eslAMINO);
+   else if (esl_opt_GetBoolean(go, "--dna"))      abc = esl_alphabet_Create(eslDNA);
+   else if (esl_opt_GetBoolean(go, "--rna"))      abc = esl_alphabet_Create(eslRNA);
 
    /* Open the .hmm file */
-   status = p7_hmmfile_OpenE(hmmfile, NULL, &cfg.hfp, errbuf);
-
-	/* Set up the configuration structure shared amongst functions here */
-	/* Determine alphabet: do what user says, else default to amino */
-   if       (esl_opt_GetBoolean(go, "--amino"))    cfg.abc = esl_alphabet_Create(eslAMINO);
-   else if  (esl_opt_GetBoolean(go, "--dna"))      cfg.abc = esl_alphabet_Create(eslDNA);
-   else if  (esl_opt_GetBoolean(go, "--rna"))      cfg.abc = esl_alphabet_Create(eslRNA);
-	else                                            cfg.abc = esl_alphabet_Create(eslAMINO);
-
-   /* Open the .hmm file */
-   status = p7_hmmfile_OpenE(hmmfile, NULL, &cfg.hfp, errbuf);
+   status = p7_hmmfile_OpenE(hmmfile, NULL, &hfp, errbuf);
    /* error handling below copied from hmmsearch */
    if      (status == eslENOTFOUND) p7_Fail("File existence/permissions problem in trying to open HMM file %s.\n%s\n", hmmfile, errbuf);
    else if (status == eslEFORMAT)   p7_Fail("File format problem in trying to open HMM file %s.\n%s\n",                hmmfile, errbuf);
    else if (status != eslOK)        p7_Fail("Unexpected error %d in opening HMM file %s.\n%s\n",               status, hmmfile, errbuf);
-	else
-	/* read the .hmm file */
-   status = p7_hmmfile_Read(cfg.hfp, &cfg.abc, &cfg.hmm);
 
-	/* read the potts file */
-	cfg.potts = pottsfile_Read(pottsfile, cfg.abc, errbuf);
+   /* read the .hmm file */
+   status = p7_hmmfile_Read(hfp, &abc, &hmm);
+   p7_hmmfile_Close(hfp);
 
-	/* combine hmm and potts object to create hpm object */
-	cfg.hpm = hpm_Create_hmm_potts(cfg.hmm, cfg.potts, cfg.abc);
+   /* read the potts file */
+   potts = pottsfile_Read(pottsfile, abc, errbuf);
 
-	/* open hpm outfile for writing, write hpm to it */
-	if ((hpm_outfp = fopen(hpm_outfile, "w")) == NULL) esl_fatal("Failed to open output hpm file %s for writing", hpm_outfile);
-	hpmfile_Write(hpm_outfp, cfg.hpm);
+   /* combine hmm and potts object to create hpm object */
+   hpm = hpm_Create_hmm_potts(hmm, potts, abc);
 
-	/* close hpm outfile */
-	fclose(hpm_outfp);
-	return 0;
+   /* open hpm outfile for writing, write hpm to it */
+   if ((hpm_outfp = fopen(hpm_outfile, "w")) == NULL) esl_fatal("Failed to open output hpm file %s for writing", hpm_outfile);
+   hpmfile_Write(hpm_outfp, hpm);
 
-	//ERROR:
-   //   return status;
+   /* close hpm outfile */
+   esl_alphabet_Destroy(abc);
+   hpm_Destroy(hpm);
+   p7_hmm_Destroy(hmm);
+   potts_Destroy(potts);
+   esl_getopts_Destroy(go);
+   fclose(hpm_outfp);
+   return 0;
 
 }
