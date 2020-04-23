1f4fd8e9392924c0e639d78e2f1ff1c04bdefb25
gwwilburn <wilburn@g.harvard.edu>
2020-04-22 22:36:59 -0400

    added destroy functions to hpm_scoreset and retabbed

--- src/hpm_scoreset.c
+++ src/hpm_scoreset.c
@@ -4,10 +4,12 @@
 #include "hmmer.h"
 #include "easel.h"
 #include "esl_alphabet.h"
+#include "esl_arr2.h"
 #include "esl_composition.h"
 
+#include <string.h>
 
-/* Function hpm_scoreset_Create()
+/* Function ahpm_scoreset_Create()
  *
  * Synopsis: Allocate score set object for original (viterbi path) hpm scoring method
  *
@@ -21,39 +23,39 @@
 HPM_SCORESET *
 hpm_scoreset_Create(int nseq)
 {
-	HPM_SCORESET *hpm_ss = NULL;
-	int           n;
-	int           status;
+   HPM_SCORESET *hpm_ss = NULL;
+   int           n;
+   int           status;
 
-	ESL_ALLOC(hpm_ss, sizeof(HPM_SCORESET));
+   ESL_ALLOC(hpm_ss, sizeof(HPM_SCORESET));
 
-	/* set number of sequences */
-	hpm_ss->nseq = nseq;
+   /* set number of sequences */
+   hpm_ss->nseq = nseq;
 
-	/* allocate memory for sequence name array */
-	ESL_ALLOC(hpm_ss->sqname, sizeof(char *) * nseq);
+   /* allocate memory for sequence name array */
+   ESL_ALLOC(hpm_ss->sqname, sizeof(char *) * nseq);
 
-	/* allocate memory for probabilities/scores */
-	ESL_ALLOC(hpm_ss->E_hi,         sizeof(float) * nseq);
-	ESL_ALLOC(hpm_ss->E_eij,        sizeof(float) * nseq);
-	ESL_ALLOC(hpm_ss->lp_ins,       sizeof(float) * nseq);
-	ESL_ALLOC(hpm_ss->lp_trans,     sizeof(float) * nseq);
-	ESL_ALLOC(hpm_ss->lpnull_match, sizeof(float) * nseq);
-	ESL_ALLOC(hpm_ss->lpnull_trans, sizeof(float) * nseq);
+   /* allocate memory for probabilities/scores */
+   ESL_ALLOC(hpm_ss->E_hi,         sizeof(float) * nseq);
+   ESL_ALLOC(hpm_ss->E_eij,        sizeof(float) * nseq);
+   ESL_ALLOC(hpm_ss->lp_ins,       sizeof(float) * nseq);
+   ESL_ALLOC(hpm_ss->lp_trans,     sizeof(float) * nseq);
+   ESL_ALLOC(hpm_ss->lpnull_match, sizeof(float) * nseq);
+   ESL_ALLOC(hpm_ss->lpnull_trans, sizeof(float) * nseq);
 
-	for (n = 0; n < nseq; n++) {
-		hpm_ss->E_hi[n]         = 0.0;
-		hpm_ss->E_eij[n]        = 0.0;
-		hpm_ss->lp_ins[n]       = 0.0;
-		hpm_ss->lp_trans[n]     = 0.0;
-		hpm_ss->lpnull_match[n] = 0.0;
-		hpm_ss->lpnull_trans[n] = 0.0;
-	}
+   for (n = 0; n < nseq; n++) {
+      hpm_ss->E_hi[n]         = 0.0;
+      hpm_ss->E_eij[n]        = 0.0;
+      hpm_ss->lp_ins[n]       = 0.0;
+      hpm_ss->lp_trans[n]     = 0.0;
+      hpm_ss->lpnull_match[n] = 0.0;
+      hpm_ss->lpnull_trans[n] = 0.0;
+   }
 
-	return hpm_ss;
+   return hpm_ss;
 
-	ERROR:
-		return NULL;
+   ERROR:
+      return NULL;
 }
 
 
@@ -73,39 +75,81 @@ hpm_scoreset_Create(int nseq)
 HPM_IS_SCORESET *
 hpm_is_scoreset_Create(int nseq)
 {
-	HPM_IS_SCORESET *hpm_is_ss = NULL;
-	int              n;
-	int              status;
+   HPM_IS_SCORESET *hpm_is_ss = NULL;
+   int              n;
+   int              status;
 
-	ESL_ALLOC(hpm_is_ss, sizeof(HPM_IS_SCORESET));
+   ESL_ALLOC(hpm_is_ss, sizeof(HPM_IS_SCORESET));
 
-	/* set number of sequences */
-	hpm_is_ss->nseq = nseq;
+   /* set number of sequences */
+   hpm_is_ss->nseq = nseq;
 
-	/* allocate memory for sequence name array */
-	ESL_ALLOC(hpm_is_ss->sqname, sizeof(char *) * nseq);
+   /* allocate memory for sequence name array */
+   ESL_ALLOC(hpm_is_ss->sqname, sizeof(char *) * nseq);
 
-	/* allocate memory for probabilities/scores */
-	ESL_ALLOC(hpm_is_ss->R,           sizeof(int)   * nseq);
-	ESL_ALLOC(hpm_is_ss->H,           sizeof(float) * nseq);
-	ESL_ALLOC(hpm_is_ss->fwd,         sizeof(float) * nseq);
-	ESL_ALLOC(hpm_is_ss->is_ld,       sizeof(float) * nseq);
+   /* allocate memory for probabilities/scores */
+   ESL_ALLOC(hpm_is_ss->R,           sizeof(int)   * nseq);
+   ESL_ALLOC(hpm_is_ss->H,           sizeof(float) * nseq);
+   ESL_ALLOC(hpm_is_ss->fwd,         sizeof(float) * nseq);
+   ESL_ALLOC(hpm_is_ss->is_ld,       sizeof(float) * nseq);
 
+   for (n = 0; n < nseq; n++) {
+      hpm_is_ss->R[n]         = 0;
+      hpm_is_ss->H[n]         = 0.0;
+      hpm_is_ss->fwd[n]       = 0.0;
+      hpm_is_ss->is_ld[n]     = 0.0;
+   }
 
-	for (n = 0; n < nseq; n++) {
-		hpm_is_ss->R[n]         = 0;
-		hpm_is_ss->H[n]         = 0.0;
-		hpm_is_ss->fwd[n]       = 0.0;
-		hpm_is_ss->is_ld[n]     = 0.0;
-	}
+   return hpm_is_ss;
 
-	return hpm_is_ss;
-
-	ERROR:
-		return NULL;
+   ERROR:
+      return NULL;
 }
 
+/* Function:  hpm_scoreset_Destroy()
+ * Synopsis:  Free an <HPM_SCORESET>
+ *
+ * Purpose:   Frees the body of an <HPM_SCORESET>
+ *
+ * Note:      Based on p7_hmm_Destroy()
+ *
+ * Returns:   (void).
+ */
 
+void
+hpm_scoreset_Destroy(HPM_SCORESET *hpm_ss) {
+
+   if (hpm_ss == NULL) return;
+
+   free(hpm_ss);
+
+   return;
+}
+
+/* Function:  hpm_is_scoreset_Destroy()
+ * Synopsis:  Free an <HPM_IS_SCORESET>
+ *
+ * Purpose:   Frees the body of an <HPM_IS_SCORESET>
+ *
+ * Note:      Based on p7_hmm_Destroy()
+ *
+ * Returns:   (void).
+ */
+
+void
+hpm_is_scoreset_Destroy(HPM_IS_SCORESET *hpm_is_ss) {
+
+   if (hpm_is_ss == NULL) return;
+
+   if (hpm_is_ss->sqname)  free(hpm_is_ss->sqname);
+   if (hpm_is_ss->R)       free(hpm_is_ss->R);
+   if (hpm_is_ss->H)       free(hpm_is_ss->H);
+   if (hpm_is_ss->fwd)     free(hpm_is_ss->fwd);
+   if (hpm_is_ss->is_ld)   free(hpm_is_ss->is_ld);
+
+   free(hpm_is_ss);
+   return;
+}
 
 /* Function hpm_scoreset_Write()
  *
@@ -119,30 +163,27 @@ hpm_is_scoreset_Create(int nseq)
 
 int
 hpm_scoreset_Write(FILE *fp, HPM_SCORESET *hpm_ss){
-	int n;
+   int n; /* sequence index */
 
-	/* write csv header line */
+   /* write csv header line */
+   fprintf(fp,"id,E_hi,E_eij,hamiltonian,lp_ins,lp_trans,lpnull_match,lpnull_trans,logodds_unnormalized\n");
 
-	fprintf(fp,"id,E_hi,E_eij,hamiltonian,lp_ins,lp_trans,lpnull_match,lpnull_trans,logodds_unnormalized\n");
+   /* loop over sequences, print id and scores */
+   for (n = 0; n < hpm_ss->nseq; n++) {
+      fprintf(fp, "%s,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n",
+              hpm_ss->sqname[n],
+              hpm_ss->E_hi[n],
+              hpm_ss->E_eij[n],
+              hpm_ss->E_hi[n] + hpm_ss->E_eij[n],
+              hpm_ss->lp_ins[n],
+              hpm_ss->lp_trans[n],
+              hpm_ss->lpnull_match[n],
+              hpm_ss->lpnull_trans[n],
+              hpm_ss->E_hi[n] + hpm_ss->E_eij[n] + hpm_ss->lp_trans[n] - hpm_ss->lpnull_match[n] - hpm_ss->lpnull_trans[n]);
+   }
 
-	/* loop over sequences, print id and scores */
-	for (n = 0; n < hpm_ss->nseq; n++) {
-		fprintf(fp, "%s,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n",
-				  hpm_ss->sqname[n],
-				  hpm_ss->E_hi[n],
-				  hpm_ss->E_eij[n],
-				  hpm_ss->E_hi[n] + hpm_ss->E_eij[n],
-				  hpm_ss->lp_ins[n],
-				  hpm_ss->lp_trans[n],
-				  hpm_ss->lpnull_match[n],
-				  hpm_ss->lpnull_trans[n],
-				  hpm_ss->E_hi[n] + hpm_ss->E_eij[n] + hpm_ss->lp_trans[n] - hpm_ss->lpnull_match[n] - hpm_ss->lpnull_trans[n]);
-
-	}
-
-	return eslOK;
+   return eslOK;
 }
-
 
 /* Function hpm_is_scoreset_Write()
  *
@@ -156,22 +197,20 @@ hpm_scoreset_Write(FILE *fp, HPM_SCORESET *hpm_ss){
 
 int
 hpm_is_scoreset_Write(FILE *fp, HPM_IS_SCORESET *hpm_is_ss){
-	int n;
+   int n; /* sequence index */
 
-	/* write csv header line */
+   /* write csv header line */
+   fprintf(fp,"id,N_sample,path_ent,fwd_logodds,HPM_IS_logodds\n");
 
-	fprintf(fp,"id,N_sample,path_ent,fwd_logodds,HPM_IS_logodds\n");
+   /* loop over sequences, print id and scores */
+   for (n = 0; n < hpm_is_ss->nseq; n++) {
+      fprintf(fp, "%s,%d,%.4f,%.4f,%.4f\n",
+              hpm_is_ss->sqname[n],
+              hpm_is_ss->R[n],
+              hpm_is_ss->H[n],
+              hpm_is_ss->fwd[n],
+              hpm_is_ss->is_ld[n]);
+   }
 
-	/* loop over sequences, print id and scores */
-	for (n = 0; n < hpm_is_ss->nseq; n++) {
-		fprintf(fp, "%s,%d,%.4f,%.4f,%.4f\n",
-				  hpm_is_ss->sqname[n],
-				  hpm_is_ss->R[n],
-				  hpm_is_ss->H[n],
-				  hpm_is_ss->fwd[n],
-				  hpm_is_ss->is_ld[n]);
-
-	}
-
-	return eslOK;
+   return eslOK;
 }
--- src/hpm_scoreset.h
+++ src/hpm_scoreset.h
@@ -10,38 +10,39 @@
 #include "easel.h"
 #include "esl_alphabet.h"
 #include "potts.h"
+#include "esl_arr2.h"
 
 typedef struct hpmscoreset_s{
-	char   **sqname;        /* sequence names [0..nseq-1][], \0-terminated            */
-	int 		nseq;          /* number of sequences                                    */
-	float   *E_hi;          /* Potts h_i pseudo-energies [0...nseq-1]                 */
-	float   *E_eij;         /* Potts e_ij pseudo-energies [0...nseq-1]                */
-	float   *lp_ins;        /* product of insertion probabilities [0...nseq-1]        */
-	float   *lp_trans;      /* product of transition probabilities [0...nseq-1]       */
-	float   *lpnull_match;  /* null model probabilities of match states [0...nseq-1]  */
-	float   *lpnull_trans;  /* null model probabilities of transitions  [0...nseq-1]  */
-
+   char   **sqname;        /* sequence names [0..nseq-1][], \0-terminated            */
+   int      nseq;          /* number of sequences                                    */
+   float   *E_hi;          /* Potts h_i pseudo-energies [0...nseq-1]                 */
+   float   *E_eij;         /* Potts e_ij pseudo-energies [0...nseq-1]                */
+   float   *lp_ins;        /* product of insertion probabilities [0...nseq-1]        */
+   float   *lp_trans;      /* product of transition probabilities [0...nseq-1]       */
+   float   *lpnull_match;  /* null model probabilities of match states [0...nseq-1]  */
+   float   *lpnull_trans;  /* null model probabilities of transitions  [0...nseq-1]  */
 } HPM_SCORESET;
 
 
 typedef struct hpmisscoreset_s{
-	char   **sqname;        /* sequence names [0..nseq-1][], \0-terminated            */
-	int 		nseq;          /* number of sequences                                    */
-	int     *R;             /* number of stochastic traces                            */
-	float   *H;             /* Posterior path entropies H(pi | x)                     */
-	float   *fwd;           /* forward log-odds scores                                */
-	float   *is_ld;         /* unnormalized importance sampling log-odds scores       */
-
+   char   **sqname;        /* sequence names [0..nseq-1][], \0-terminated            */
+   int      nseq;          /* number of sequences                                    */
+   int     *R;             /* number of stochastic traces                            */
+   float   *H;             /* Posterior path entropies H(pi | x)                     */
+   float   *fwd;           /* forward log-odds scores                                */
+   float   *is_ld;         /* unnormalized importance sampling log-odds scores       */
 
 } HPM_IS_SCORESET;
 
 
 
-/* hpm_scoreset.c	*/
-extern HPM_SCORESET	   *hpm_scoreset_Create(int nseq);
-extern HPM_IS_SCORESET	*hpm_is_scoreset_Create(int nseq);
-extern int				    hpm_scoreset_Write(FILE *fp, HPM_SCORESET *hpm_ss);
-extern int				    hpm_is_scoreset_Write(FILE *fp, HPM_IS_SCORESET *hpm_is_ss);
+/* hpm_scoreset.c */
+extern HPM_SCORESET     *hpm_scoreset_Create(int nseq);
+extern HPM_IS_SCORESET  *hpm_is_scoreset_Create(int nseq);
+extern void              hpm_scoreset_Destroy(HPM_SCORESET *hpm_ss);
+extern void              hpm_is_scoreset_Destroy(HPM_IS_SCORESET *hpm_is_ss);
+extern int               hpm_scoreset_Write(FILE *fp, HPM_SCORESET *hpm_ss);
+extern int               hpm_is_scoreset_Write(FILE *fp, HPM_IS_SCORESET *hpm_is_ss);
 
 
 #endif
