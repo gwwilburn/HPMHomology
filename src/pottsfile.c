af56c20a2cb099484464ec6f095edf5ea6b0e05c
gwwilburn <wilburn@g.harvard.edu>
2020-04-22 23:13:44 -0400

    retabbed, fixed compile warnings, and removed cruft in pottsfile.c and pottsfile.h

--- src/pottsfile.c
+++ src/pottsfile.c
@@ -15,103 +15,92 @@
 
 POTTS *
 pottsfile_Read(char *f, ESL_ALPHABET *abc, char *errbuf){
+   ESL_FILEPARSER  *efp           = NULL;      /* easel fileparse object */
+   POTTS           *ret_potts     = NULL;      /* potts model to return  */
+   char            *tok;                       /* file parser token      */
+   char            *prev_tok      = "blargh";  /* file parser token      */
+   int              tok_count;                 /* token count            */
+   int              i;                         /* position index         */
+   int              prev_i;                    /* position index         */
+   int              j;                         /* position index         */
+   int              a;                         /* alphabet index         */
+   int              lc            = 0;         /* line counter           */
+   int              section_count = 0;         /* file section counter   */
+   int              status;
 
-	ESL_FILEPARSER  *efp           = NULL;
-	POTTS           *ret_potts     = NULL;
-	char            *tok;
-	char            *prev_tok      = "blargh";
-	int              tok_count;
-	int              i;                        /* position index           */
-	int              prev_i;                   /* position index           */
-	int              j;                        /* position index           */
-	int              a;                        /* alphabet index           */
-	int              lc            = 0;		    /* line counter             */
-	int				  section_count = 0;
-	int              status;
+   if ((status = esl_fileparser_Open(f, NULL, &efp)) != eslOK) ESL_XFAIL(eslFAIL, errbuf,
+                                                               "pottsfile_Read(): file open failed w/ status %s\n", status);
 
-	if (esl_fileparser_Open(f, NULL, &efp) != eslOK)  ESL_XFAIL(eslFAIL, errbuf, "file open failed");
+   /* loop through one to get model length */
+   while (esl_fileparser_NextLine(efp) == eslOK) {
+      if (section_count == 0) {
+         tok_count = 0;
 
-	/* loop through one to get model length */
-	while (esl_fileparser_NextLine(efp) == eslOK) {
-		if (section_count == 0) {
-			tok_count = 0;
-			while (esl_fileparser_GetTokenOnLine(efp, &tok, NULL) == eslOK) {
+         while (esl_fileparser_GetTokenOnLine(efp, &tok, NULL) == eslOK) {
 
-				/* if the 2nd character is an integer, we've reached the end of the h_i section */
-				if ( (tok_count == 1) && (IsInt(tok) > 0) && (IsInt(prev_tok))) {
-					section_count = 1;
-					ret_potts = potts_Create(prev_i+1, abc);
-					break;
-				}
+            /* if the 2nd character is an integer, we've reached the end of the h_i section */
+            if ((tok_count == 1) && (IsInt(tok) > 0) && (IsInt(prev_tok))) {
+               section_count = 1;
+               ret_potts = potts_Create(prev_i+1, abc);
+               break;
+            }
 
-				if (tok_count == 0)
-				{
-					i = atoi(tok);
-				}
+            if (tok_count == 0) i = atoi(tok);
+            tok_count++;
+            prev_tok = tok;
+         }
+         prev_i = i;
+      }
+   }
 
-				tok_count++;
-		 		prev_tok = tok;
-			}
-		prev_i = i;
+   esl_fileparser_Close(efp);
+   fprintf(stdout, "%d\n", ret_potts->L);
 
-		}
-	}
+   /* loop through again to get potts parameters */
+   if ((status = esl_fileparser_Open(f, NULL, &efp)) != eslOK) ESL_XFAIL(eslFAIL, errbuf,
+                                                               "pottsfile_Read(): file open failed w/ status %s\n", status);
 
-	esl_fileparser_Close(efp);
-	fprintf(stdout, "%d\n", ret_potts->L);
+   while (esl_fileparser_NextLine(efp) == eslOK) {
+      /* we are in the h_i section */
+      if (lc < ret_potts->L) {
+         tok_count = 0;
 
-	/* loop through again to get potts parameters */
- 	if (esl_fileparser_Open(f, NULL, &efp) != eslOK)  ESL_XFAIL(eslFAIL, errbuf, "file open failed");
+         while (esl_fileparser_GetTokenOnLine(efp, &tok, NULL) == eslOK) {
 
-	while (esl_fileparser_NextLine(efp) == eslOK) {
-		/* we are in the h_i section */
-		if (lc < ret_potts->L)
-		{
-			tok_count = 0;
-			while (esl_fileparser_GetTokenOnLine(efp, &tok, NULL) == eslOK) {
-				if (tok_count == 0)
-				{
-					i = atoi(tok);
-				}
-				else {
-					ret_potts->h[i][tok_count-1] = atof(tok);
-				}
-				tok_count += 1;
-			}
+            if   (tok_count == 0) i = atoi(tok);
+            else                  ret_potts->h[i][tok_count-1] = atof(tok);
 
+            tok_count += 1;
+         }
+      }
 
-		}
-		/* we are in e_ij section */
-		else {
-			tok_count = 0;
-			while (esl_fileparser_GetTokenOnLine(efp, &tok, NULL) == eslOK) {
+      /* we are in e_ij section */
+      else {
+         tok_count = 0;
+         while (esl_fileparser_GetTokenOnLine(efp, &tok, NULL) == eslOK) {
+            /* if the line starts with two integers, those are i and j */
+            if ( (tok_count == 1) && (IsInt(tok)) && (IsInt(prev_tok))) {
+               i = atoi(prev_tok);
+               j = atoi(tok);
+               a = -1;
+            }
 
-				/* if the line starts with two integers, those are i and j */
-				if ( (tok_count == 1) && (IsInt(tok)) && (IsInt(prev_tok))) {
-					i = atoi(prev_tok);
-					j = atoi(tok);
-					a = -1;
-				}
-				/* if the token is a float, we have a potts parameter */
-				else if (IsInt(tok) == 0) {
-					ret_potts->e[i][j][IDX(a,tok_count,abc->K+1)] = atof(tok);
-					ret_potts->e[j][i][IDX(tok_count,a,abc->K+1)] = atof(tok);
-				}
-			tok_count ++;
-			prev_tok = tok;
-			}
-			a++;
-		}
-		lc++;
+            /* if the token is a float, we have a potts parameter */
+            else if (IsInt(tok) == 0) {
+               ret_potts->e[i][j][IDX(a,tok_count,abc->K+1)] = atof(tok);
+               ret_potts->e[j][i][IDX(tok_count,a,abc->K+1)] = atof(tok);
+            }
+            tok_count ++;
+            prev_tok = tok;
+         }
+         a++;
+      }
+      lc++;
+   }
 
+   esl_fileparser_Close(efp);
+   return ret_potts;
 
-	}
-
-	esl_fileparser_Close(efp);
-
-	return ret_potts;
-
-	ERROR:
-		return NULL;
-
+   ERROR:
+      return NULL;
 };
--- src/pottsfile.h
+++ src/pottsfile.h
@@ -10,13 +10,7 @@
 
 #include "potts.h"
 
-typedef struct pottsfile{
-	FILE     *f;      /* pointer to stream for reading models      */
-	char     *fname;  /* name of the hpm file                      */
-} POTTSFILE;
-
 /* pottsfile.c */
 POTTS *pottsfile_Read(char *f, ESL_ALPHABET *abc, char *errbuff);
-
 
 #endif
